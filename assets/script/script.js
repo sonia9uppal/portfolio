7da31c071644718f248664cb91cf01c1f1839ffa
soniauppal <sonia9uppal@gmail.com>
2017-11-26 15:13:53 -0800

    second landing page done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-    sectionsColor : ['#CCD1D1', '#fff', '#000'],
+    sectionsColor: ['#CCD1D1', '#fff', '#000'],
     menu: '#menu',
     anchors: ['Home', 'Developing', 'Design'],
     loopBottom: true,
@@ -8,8 +8,34 @@ $(document).ready(function() {
     navigationTooltips: ['Home', 'Developing', 'Design']
   });
 
-  /*$('#facebook-logo').click({
-    $(this).attr('href','http://facebook.com/soniaupp');
-  });*/
+  $('.logo').hover(function() { //jQuery Function Number 1
+    $(this).css('opacity', '0.7'); //jQuery Function Number 2
+  }, function() {
+    $(this).css('opacity', '1');
+  });
+
+  $("#berkeley").hover(function() {
+    $("#1p").css('opacity', '1');
+  }, function() {
+    $("#1p").css('opacity', '0');
+  });
+
+  $("#adobe").hover(function() {
+    $("#2p").css('opacity', '1');
+  }, function() {
+    $("#2p").css('opacity', '0');
+  });
+
+  $("#whatsapp").hover(function() {
+    $("#3p").css('opacity', '1');
+  }, function() {
+    $("#3p").css('opacity', '0');
+  });
+
+  $("#pialacode").hover(function() {
+    $("#palcp").css('opacity', '1');
+  }, function() {
+    $("#palcp").css('opacity', '0');
+  });
 
 });
